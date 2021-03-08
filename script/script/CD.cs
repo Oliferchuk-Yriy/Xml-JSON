1ab524b6c3375e9f80cf6888972979f5854084c8
Oliferchuk-Yuriy <80285133+Oliferchuk-Yriy@users.noreply.github.com>
2021-03-08 21:42:59 +0200

    Add files via upload

---
+++ script/script/CD.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace script
+{
+    [Serializable]
+    [XmlRoot("CATALOG")]
+    public class Catalog
+    {
+        [XmlElement("CD")]
+        public CD[] Cd { get; set; }
+    }
+    [Serializable]
+    public class CD
+    {
+        [XmlElement("TITLE")]
+        public string Title { get; set; }
+
+        [XmlElement("ARTIST")]
+        public string Artist { get; set; }
+
+        [XmlElement("COUNTRY")]
+        public string Country { get; set; }
+
+        [XmlElement("COMPANY")]
+        public string Company { get; set; }
+
+        [XmlElement("PRICE")]
+        public float Price { get; set; }
+
+        [XmlElement("YEAR")]
+        public int Year { get; set; }
+    }
+}
